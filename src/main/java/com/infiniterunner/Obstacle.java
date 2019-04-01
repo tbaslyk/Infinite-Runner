cadae54f5d5007834afa1db66b056362e1bdfeda
Tomasz <tomaszbaslykit@gmail.com>
2019-04-01 09:50:33 +0200

    initial

---
+++ src/main/java/com/infiniterunner/Obstacle.java
@@ -0,0 +1,17 @@
+package com.infiniterunner;
+
+/**
+ * @author Tomasz Baslyk
+ */
+public class Obstacle {
+
+    int x;
+    int y;
+    int w;
+    int h;
+
+    public Obstacle() {
+
+    }
+
+}
